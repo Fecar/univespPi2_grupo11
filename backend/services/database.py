35bd37f0b9f845df8e54d69bfe29489ffb2aade3
Felipe Caetano <fecae95@gmail.com>
2026-09-09 00:11:07 -0300

    first: adding Docker architecture

---
+++ backend/services/database.py
@@ -0,0 +1,22 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class DatabasePSQL:
+
+    def __init__(self):
+        self.__user = os.getenv("DB_USER")
+        self.__pass = os.getenv("DB_PASSWD")
+        self.__db = os.getenv("DB_NAME")
+
+    def get_db_conn(self):
+        conn = psycopg2.connect(
+            host="db",
+            database=self.__db,
+            user=self.__user,
+            password=self.__pass
+        )
+        return conn
