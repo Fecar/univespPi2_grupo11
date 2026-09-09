672f9259d16b3d1063d6f8713c8efd57892fe526
Felipe Caetano <fecae95@gmail.com>
2026-09-09 00:42:18 -0300

    feat: configuração do docker, Banco de dados e API Flask

--- backend/services/database.py
+++ backend/services/database.py
@@ -9,7 +9,7 @@ class DatabasePSQL:
 
     def __init__(self):
         self.__user = os.getenv("DB_USER")
-        self.__pass = os.getenv("DB_PASSWD")
+        self.__pass = os.getenv("DB_PASSWORD")
         self.__db = os.getenv("DB_NAME")
 
     def get_db_conn(self):
